67ba85f5913a1aa228edcd92b2f8efe4ed94eca6
sonilboss <sonil.jamshidi1@gmail.com>
2023-03-17 08:45:52 +0100

    it draws stuff now
    yes tyews

--- Tile.js
+++ Tile.js
@@ -1,16 +1,5 @@
 class Tile {
-    constructor(x,y,sprite,width,height){
+    constructor(sprite){
    this.sprite=sprite
-   this.x=x
-   this.y=y
-   this.width=width
-   this.height=height
     }
-    display(){
-        stroke(244)
-        strokeWeight(3)
-        noFill()
-        image(this.sprite,this.x,this.y)
-    }
-
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
-let puzles
 const tiles=[]
 let pictures =[]
 
+
 function preload (){
-  for (let i=0;i<=15;i++){
-    for (let j=0;j<=15;j++){
-  let image = loadImage("pictures/img"+i+".png")
+  for (let i=0;i<15;i++){
+  let image = loadImage("pictures/img"+(i)+".png")
   pictures.push(image)
-  tiles.push(new Tile(400 * i ,400 * j,random(pictures),windowWidth/4,windowHeight/4))
+  tiles.push(new Tile(pictures[i]))
+
     }
   }
-}
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,8 +18,15 @@ function setup() {
 
 function draw() {
   background(220);
+let x=0
+let y=0
+for (let Tile of tiles){
 
-  for (let Tile of tiles){
-    Tile.display();
+  image(Tile.sprite,x,y,width/4,height/4)
+  x+=width/4
+  if(x>=width){
+    x=0
+    y+=height/4
   }
+} 
 }
